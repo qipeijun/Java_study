1e4e660c9cc107dfb0b62295d2521830ee8f3c67
戚培俊 <qipeijun@aliyun.com>
2020-05-28 23:39:42 +0800

    add

--- src/com/hello/Demo8_extends.java
+++ src/com/hello/Demo8_extends.java
@@ -23,6 +23,7 @@ public class Demo8_extends {
 class Animal {
     String color;  // 动物的颜色
     int leg;  // 动物腿的个数
+    int age;
 
     public void eat(){
         System.out.println("吃饭");
@@ -34,6 +35,7 @@ class Animal {
 }
 
 class Cat extends Animal{
+    int age; // 父类已经定义完了，子类在定义已经没有意义了
 //    String color;  // 动物的颜色
 //    int leg;  // 动物腿的个数
 //
@@ -48,13 +50,21 @@ class Cat extends Animal{
 
 class Dog extends Animal{
 
+
+    public Dog(){
+        super(); // 这是一条语句，如果不写，系统默认会加上，用来访问父类中的空参构造
+    }
+
     public void show(){
+        // this 调用的是本类的， super 调用的父类的
         System.out.println(this.color+"..."+this.leg);
+        System.out.println(super.color+"..."+super.leg);
     }
 
 }
 
 /**
+ *
  * 继承的好处 :
  * 提高了代码的复用性
  * 提高的代码的维护性
@@ -73,5 +83,15 @@ class Dog extends Animal{
  * java中类的继承特点
  * 如果想用这个体系中的所有功能用最底层的类创建对象
  * 如果想看这个体系的共性功能，看最顶层的类
+ *
+ * 继承的注意事项
+ * 子类只能继承父类所有非私有的成员(成员方法和变量)
+ * 子类不能继承父类的构造方法，但是可以通过super 关键字去访问父类构造方法
+ * 不要为了部分功能而去继承
+ *
+ * 父类没有无参的构造方法，子类怎么办？
+ * super 解决
+ * this 解决
+ * 注意事项 ： super(..) 或者 this(...) 必须出现在构造方法的第一天语句上
  *
  */
