6daf0ad4c6714752e236b23db6efe75255986eae
戚培俊 <qipeijun@aliyun.com>
2020-06-01 23:05:17 +0800

    add

--- src/com/hello/Demo9.java
+++ src/com/hello/Demo9.java
@@ -11,6 +11,21 @@ public class Demo9 {
         // 父类引用 指向 子类对象
         Animalss animalss = new Catss();
         animalss.eat();
+//        强制类型转换(不强制转换不能使用子类的属性和方法)
+        Catss ct = (Catss)animalss;
+        ct.show();
+
+        Chouxiang chouxiang = new Chouxiang() {
+            @Override
+            public void show() {
+               System.out.println("抽象类方法");
+            }
+        };
+
+        chouxiang.show();
+        chouxiang.speak();
+
+
 
     }
 
@@ -29,20 +44,53 @@ public class Demo9 {
  */
 
 
-class Animalss {
+abstract class Animalss {
 
-    public void eat(){
-        System.out.println("动物吃饭");
-    }
+    public Animalss(){};
+
+    public abstract void eat();
+
+//    public void eat(){
+//        System.out.println("动物吃饭");
+//    }
 
 }
 
 class Catss extends Animalss {
+
+    public Catss(){};
+
     @Override
     public void eat() {
 //        super.eat();
         System.out.println("猫吃鱼");
     }
+
+    public void show(){
+        System.out.println("show");
+    }
 }
 
-// TODO 9-5 节
+/**
+ *
+ * 多态向上 转型
+ * 向下转型 (Catss)强转 向下转型
+ *
+ */
+
+
+
+/**
+ * 抽象类 abstract
+ * 抽象类不能被实例化
+ * 抽象方法 强制子类要做的事
+ */
+abstract class Chouxiang {
+    // 抽象方法,子类需要强制重新写
+    public abstract void show();
+
+    public void speak(){
+        System.out.println("说句话");
+    }
+
+}
