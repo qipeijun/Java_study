7a88557c7f01b58eb77f03322b4838fa3e848ec8
戚培俊 <qipeijun@aliyun.com>
2020-06-02 21:58:10 +0800

    add

--- src/com/hello/Demo9_interface.java
+++ src/com/hello/Demo9_interface.java
@@ -2,7 +2,7 @@ package com.hello;
 
 public class Demo9_interface {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("demo 9 interface");
 
         // 接口不能被实例化，没有意义
@@ -10,33 +10,72 @@ public class Demo9_interface {
 
         Demo9_inter demo9_inter = new Demo9_inter();
         demo9_inter.print();
+        demo9_inter.show();
     }
 }
 
 
 /**
- *
  * 对外提供规则的都是接口
- *
+ * <p>
  * 接口用interface 关键字表示
- *
+ * <p>
  * 接口的子类，可以是抽象类，但是意义不大
  * 可以的具体类，要重写接口中所有的抽象方法 （推荐）
+ * <p>
+ * 成员变量 只能是常量，并且是静态的并公共的，
+ * 默认修饰符 ： public static final 三个关键字可以相互交换，建议手动给出
+ * <p>
+ * 构造方法 接口中没有构造方法
+ * <p>
+ * 一个接口不写继承任何类，默认继承Object
  *
+ * 接口与接口的关系，可以单继承，也可以多继承
  */
-
 
 
 interface Inter {
     // 接口中的方法都是抽象的
     public abstract void print();
+
+    public abstract void show();
+
+    public static final int num = 10;
+
+//    public void prints(){} // 接口中不能定义非抽象方法
+
+//    public Inter(){} // 接口中没有构造方法
 }
 
-class Demo9_inter implements Inter{
+interface InterA {
+    public abstract void showMe();
+}
+
+// 接口继承，可以多继承
+interface InterB extends InterA,Inter{
+
+}
+
+class Demo9_inter implements Inter,InterA {
     @Override
     public void print() {
+        System.out.println(num);
         System.out.println("接口");
+    }
+
+    @Override
+    public void show() {
+        System.out.println("重写show方法");
+    }
+
+    @Override
+    public void showMe() {
+        System.out.println("重写show ME");
     }
 }
 
-// todo 9-17
+
+
+
+
+
---
+++ src/com/hello/Demo9_study.java
@@ -0,0 +1,93 @@
+package com.hello;
+
+public class Demo9_study {
+
+    public static void main(String[] args) {
+        System.out.println("demo 9 study");
+
+        Cat1 cat1 = new Cat1("蓝猫", 1);
+        cat1.hige();
+
+        Dog1 dog1 = new Dog1("哈士奇", 1);
+        dog1.hige();
+
+    }
+
+}
+
+
+class Anima {
+    String name;
+    int age;
+
+    // 空参数构造
+    public Anima() {
+    }
+
+    ;
+
+    // 有参数构造
+    public Anima(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void show() {
+        System.out.println(this.name + "。。。。" + this.age);
+    }
+}
+
+interface Hige {
+    // 跳高
+    public abstract void hige();
+}
+
+class Cat1 extends Anima implements Hige {
+
+    public Cat1() {
+    }
+
+    ;
+
+    public Cat1(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void hige() {
+        System.out.println("猫能跳50cm");
+    }
+}
+
+class Dog1 extends Anima implements Hige {
+
+    public Dog1() {
+    }
+
+    ;
+
+    public Dog1(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void hige() {
+        System.out.println("狗能跳100cm");
+    }
+}
