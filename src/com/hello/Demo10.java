e959a31e8e7329b440660882610091aea45bf4d8
戚培俊 <qipeijun@aliyun.com>
2020-06-02 23:07:25 +0800

    add

--- src/com/hello/Demo10.java
+++ src/com/hello/Demo10.java
@@ -1,10 +1,29 @@
 package com.hello;
-
+import com.world.Test;  // 导包
+import java.util.*;  // 一般开发中，不这样导入，一般都是导入具体的类
 
 public class Demo10 {
 
     public static void main(String[] args){
+
         System.out.println("demo 10");
+
+        com.hello.Cat1 cat1 = new Cat1("蓝猫",1);
+        cat1.hige();
+
+        Test test = new Test();
+        test.show();
+
+
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        System.out.println(x);
+
     }
 
+
+
 }
+
+
+// todo 10-07
--- src/com/world/Test.java
+++ src/com/world/Test.java
@@ -4,4 +4,13 @@ public class Test {
     public static void main(String[] args){
         System.out.println("test world");
     }
+
+    public void show(){
+        System.out.println("show");
+    }
+
+//  protected 修饰   在不同包下的无关类，访问不了，
+    protected void print(){
+        System.out.println("protected print");
+    }
 }
